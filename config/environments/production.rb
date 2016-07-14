cc551a0419174e8c19aac5218af5a63c65c1e68f
Phani Ramakrishna <phani@digifutura.com>
2016-07-14 16:59:08 +0530

    email setting production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.default_url_options = { :host => 'photographerr.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'photographerr.herokuapp.com', :protocol => 'http' }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -63,8 +63,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.perform_deliveries = true
+
+ config.action_mailer.delivery_method = :smtp
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
