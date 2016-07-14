fe8cb5c83969953599db91fd531ffdcc67b4d28f
Phani Ramakrishna <phani@digifutura.com>
2016-07-14 17:02:54 +0530

    email changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
       config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 587,
+    :port                 => 25,
     :domain               => "gmail.com",
     :user_name            => "phani.twintest@gmail.com",
     :password             => "sumaphani727",
